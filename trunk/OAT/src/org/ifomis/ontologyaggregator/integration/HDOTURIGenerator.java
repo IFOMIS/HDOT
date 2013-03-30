fea2102a8c611ab7c6b23e342fc54f3910d814b4
nikolina.koleva19@yahoo.de <nikolina.koleva19@yahoo.de@8d8f8329-568f-2eab-f119-087f8db2d923>
2013-03-30 11:00:57 +0000

    uri generator implemented

--- trunk/OAT/src/org/ifomis/ontologyaggregator/integration/HDOTURIGenerator.java
+++ trunk/OAT/src/org/ifomis/ontologyaggregator/integration/HDOTURIGenerator.java
@@ -12,25 +12,34 @@ import org.ifomis.ontologyaggregator.recommendation.Recommendation;
 public class HDOTURIGenerator {
 
 	private boolean includeSubclasses;
+	private Recommendation acceptedRecommendation;
 
-	public HDOTURIGenerator(Recommendation acceptedRecommendation, boolean includeSubclasses) {
+	public HDOTURIGenerator(Recommendation acceptedRecommendation,
+			boolean includeSubclasses) {
 		this.includeSubclasses = includeSubclasses;
+		this.acceptedRecommendation = acceptedRecommendation;
 	}
-	
-	public String generateURI() throws IOException{
-//		Template template = new Template("http://hdot.googlecode.com/svn/trunk/{hdotModule}#{prefix}_{counter}");
-//
-////		String counter  = FileUtils.readFileToString(new File( "data/counterForURIS"));
-//				
-//		Map map = new HashMap();
-//		map.put("hdotModule", "a");
-//		map.put("prefix", "b");
-//		map.put("counter", counter);
 
-//		System.out.println(template.expand(map));
-		//TODO implement
-		return "";
-		
+	public String generateURI() throws IOException {
+		Template template = new Template(
+				"http://hdot.googlecode.com/svn/trunk/{hdotModule}#{prefix}_{counter}");
+		String hdotModuleIRI = acceptedRecommendation.getHdotModule()
+				.getOntologyID().getOntologyIRI().toString();
+
+		String hdotModule = hdotModuleIRI.split("/")[hdotModuleIRI.length() - 1];
+
+		String prefix = hdotModule.split("\\.")[0].toUpperCase();
+
+		String counter = "";
+			counter = FileUtils.readFileToString(new File(
+					"data/counterForURIS_" + prefix));
+
+		Map<String, String> map = new HashMap<>();
+		map.put("hdotModule", hdotModule);
+		map.put("prefix", prefix);
+		map.put("counter", counter);
+
+		return template.expand(map);
 	}
 
 }
