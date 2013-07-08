df94fcad1fe245d958fd3dfb4f997f8e43a319a3
nikolina.koleva19@yahoo.de <nikolina.koleva19@yahoo.de@8d8f8329-568f-2eab-f119-087f8db2d923>
2013-07-08 21:04:37 +0000

    smtp info in properties

--- OAT/src/org/ifomis/ontologyaggregator/notifications/EmailSender.java
+++ OAT/src/org/ifomis/ontologyaggregator/notifications/EmailSender.java
@@ -1,14 +1,13 @@
 package org.ifomis.ontologyaggregator.notifications;
 
-import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.util.Properties;
 
 import org.apache.commons.mail.DefaultAuthenticator;
 import org.apache.commons.mail.Email;
 import org.apache.commons.mail.EmailException;
 import org.apache.commons.mail.SimpleEmail;
+import org.ifomis.ontologyaggregator.util.Configuration;
 
 /**
  * The EmailSender notifies the ontology curators about difficulties with the
@@ -26,45 +25,31 @@ public class EmailSender {
 	 *            the content of the mail to be sent
 	 * @throws IOException
 	 * @throws FileNotFoundException
+	 * @throws EmailException 
 	 */
 	public void sendMail(String subject, String notification)
-			throws FileNotFoundException, IOException {
-
-		// Properties properties = new Properties();
-		//
-		//
-		// properties.load(new FileInputStream("config/aggregator.properties"));
-		//
-		// String[] curators =
-		// properties.getProperty("curatorsMailAddresses").split(";");
-		//
-		// Email email = new SimpleEmail();
-		// TODO put in properties
-		// email.setHostName("smtp.googlemail.com");
-		// email.setSmtpPort(465);
-		//
-		// email.setAuthenticator(new
-		// DefaultAuthenticator("ontology.aggregator",
-		// "solution12"));
-		// email.setSSLOnConnect(true);
-		//
-		// try {
-		// email.setFrom("ontology.aggregator@gmail.com");
-		// email.setSubject(subject);
-		// email.setMsg(notification);
-		// for (int i = 0; i < curators.length; i++) {
-		// System.out.println("email send to " + curators[i]);
-		// email.addTo(curators[i]);
-		// email.send();
-		// }
-		// // email.addTo("nikolina.koleva19@yahoo.de");
-		//
-		// } catch (EmailException e) {
-		// e.printStackTrace();
-		// }
+			throws FileNotFoundException, IOException, EmailException {
+		
+		 Email email = new SimpleEmail();
+		 email.setHostName(Configuration.SMTP_HOST);
+		 email.setSmtpPort(Configuration.SMTP_PORT);
+		
+		 email.setAuthenticator(new
+		 DefaultAuthenticator(Configuration.SMTP_USERNAME,
+		 Configuration.SMTP_PASS));
+		 email.setSSLOnConnect(true);
+		
+		 email.setFrom("ontology.aggregator@gmail.com");
+		 email.setSubject(subject);
+		 email.setMsg(notification);
+		 for (int i = 0; i < Configuration.CURATORS.length; i++) {
+		 System.out.println("email send to " + Configuration.CURATORS[i]);
+		 email.addTo(Configuration.CURATORS[i]);
+		 email.send();
+		 }
 	}
 
-	public static void main(String[] args) throws FileNotFoundException, IOException {
+	public static void main(String[] args) throws FileNotFoundException, IOException, EmailException {
 		EmailSender sender = new EmailSender();
 
 		sender.sendMail("test", "this is a test mail");
